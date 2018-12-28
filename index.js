ec35d0a57b5ed19468738ce02e1ade3c71efcd8e
evilarceus <t4ils.dev@gmail.com>
2018-12-27 18:09:59 -0800

    Added support for NZXT Smart Device

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-    HuePlus: require("./lib/HuePlus") // NZXT HUE+
+    HuePlus: require("./lib/HuePlus"), // NZXT HUE+
+    NZXT_SmartDevice: require("./lib/NZXT_SmartDevice") // NZXT Smart Device in NZXT's H200i, H400i, H500i, and H700i cases
 }
